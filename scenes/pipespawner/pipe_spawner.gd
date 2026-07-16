11a74260e170f0b516140f9b89c470cd2bab4d8d
Alex Yee <yeealexi@gmail.com>
2026-07-16 12:50:47 -0700

    add max pipe offset range

--- scenes/pipespawner/pipe_spawner.gd
+++ scenes/pipespawner/pipe_spawner.gd
@@ -6,9 +6,11 @@ const PIPE_SET = preload("uid://bghq45rw1727i")
 @export var speed := -100.0
 @export var spawn_frequency := 1.0
 @export var spawn_offset_range := 100
+@export var max_diff_between_pipe_offset := 100
 @export var gap_size := 200
 
 var spawn_disabled := false
+var previous_pipe_y_offset := 0
 
 @onready var pipes: Node2D = $"../Pipes"
 @onready var timer: Timer = $Timer
@@ -30,7 +32,13 @@ func _on_timer_timeout() -> void:
 		return
 
 	var pipe_set: PipeSet = PIPE_SET.instantiate()
-	pipe_set.setup(speed, rng.randi_range(-spawn_offset_range, spawn_offset_range), gap_size)
+	var offset_y := rng.randi_range(-spawn_offset_range, spawn_offset_range)
+	var num_tries := 0
+	while abs(offset_y - previous_pipe_y_offset) >= max_diff_between_pipe_offset:
+		offset_y = rng.randi_range(-spawn_offset_range, spawn_offset_range)
+		num_tries += 1
+		assert(num_tries <= 1_000_000)
+	pipe_set.setup(speed, offset_y, gap_size)
 	pipes.add_child(pipe_set)
 
 
