bee8426bfa7e23f97528f1ac3a72aaa543982c86
Alex Yee <yeealexi@gmail.com>
2026-07-16 11:10:57 -0700

    track high score between play sessions

--- scenes/game.gd
+++ scenes/game.gd
@@ -9,11 +9,11 @@ extends Node
 @onready var sfx_player: AudioStreamPlayer2D = %SFXPlayer
 
 var score := 0
-var high_score := 0
 var parallaxes: Array[Parallax2D]
 var parallax_speeds: Array[float]
 var bird_spawn_pos: Vector2
 var seconds_since_game_over := 0.0  # used to block player input right after dying
+@onready var score_manager: ScoreManager = $ScoreManager
 
 
 func _ready() -> void:
@@ -24,11 +24,13 @@ func _ready() -> void:
 	for p in parallaxes:
 		parallax_speeds.append(p.autoscroll.x)
 	bird_spawn_pos = da_bird.position
+	
+	%UI.update_scores(score, score_manager.high_score)
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("jump"):
-		if %GameOverPanel.visible and seconds_since_game_over >= 1.5:
+		if %GameOverPanel.visible and seconds_since_game_over >= 0.5:
 			_on_game_restart()
 
 	if %GameOverPanel.visible:
@@ -54,14 +56,17 @@ func _on_player_collided() -> void:
 		p.autoscroll.x = 0
 	pipe_spawner.spawn_disabled = true
 	da_bird.input_disabled = true
-	%UI.show_game_over(score, high_score)
+	%UI.show_game_over(score, score_manager.high_score)
+	
+	if score >= score_manager.high_score:
+		score_manager.save_high_score()
 
 
 func _on_player_cleared_pipe() -> void:
 	score += 1
-	if score > high_score:
-		high_score = score
-	%UI.update_scores(score, high_score)
+	if score > score_manager.high_score:
+		score_manager.high_score = score
+	%UI.update_scores(score, score_manager.high_score)
 	%SFXPlayer.play_point_scored()
 
 
